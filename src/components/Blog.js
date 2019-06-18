29f2d5b83cfafdf67859e00678c3aeb869a3e59f
duyue <duyue_i@didiglobal.com>
2019-06-18 15:33:19 +0800

    blog & notes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 
 import Github from "../images/github.svg";
+import Webpack from "../images/webpack.svg";
+import LeetCode from '../images/leetcode.svg'
 
 import "./Blog.less";
 
@@ -11,10 +13,22 @@ class Blog extends React.PureComponent {
         <h3>Blog & Notes</h3>
         <div id="icons">
           <div className="col-3 notes">
-            <a href="#">
+            <a href="https://github.com/duyue6002/Blog">
               <Github />
             </a>
             <p>博客文章</p>
+          </div>
+          <div className="col-3 notes">
+            <a href="https://duyue6002.github.io/webpack-practice/#/">
+              <Webpack />
+            </a>
+            <p>学习 Webpack</p>
+          </div>
+          <div className="col-3 notes">
+            <a href="https://duyue6002.github.io/leetcode-solutions/#/">
+              <LeetCode />
+            </a>
+            <p>数据结构与算法</p>
           </div>
         </div>
       </div>
